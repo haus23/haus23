1c0e0f47026ea6f00cfc37a638decb30b9a72e56
Micha Alt <micha@haus23.net>
2016-10-17 21:49:30 +0200

    Shows current ranking.

--- src/AppBundle/Controller/Dtp/Standings/RankingController.php
+++ src/AppBundle/Controller/Dtp/Standings/RankingController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller\Dtp\Standings;
 
+use AppBundle\Entity\Config;
+use AppBundle\Entity\Turnier;
+use Doctrine\ORM\QueryBuilder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,6 +15,28 @@ class RankingController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('dtp/standings/ranking.html.twig');
+        $configRepository = $this->getDoctrine()->getRepository('AppBundle:Config');
+        $championshipRepository = $this->getDoctrine()->getRepository('AppBundle:Turnier');
+
+        /** @var Config $configEntry */
+        $configEntry = $configRepository->findOneBy(['key' => 'turnier']);
+        $championshipId = $configEntry->getValue();
+
+        /** @var Turnier $championship */
+        $championship = $championshipRepository->find($championshipId);
+
+        /** @var QueryBuilder $qb */
+        $qb = $this->getDoctrine()->getManager()->createQueryBuilder();
+        $qb->select('p')
+            ->from('AppBundle:Spieler', 'p')
+            ->where('p.turnierId = ?1')
+            ->orderBy('p.platz', 'ASC')
+            ->setParameter(1, $championshipId);
+        $players = $qb->getQuery()->execute();
+
+        return $this->render('dtp/standings/ranking.html.twig', [
+            'championship' => $championship,
+            'players' => $players
+        ]);
     }
 }
