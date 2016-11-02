0769f7d002399d541463e5c334a4acc12f11f525
Micha Alt <micha@haus23.net>
2016-11-02 21:40:30 +0100

    Initial toolchain ready.

--- app/Resources/scripts/app.js
+++ app/Resources/scripts/app.js
@@ -3,5 +3,5 @@ var angular = require('angular');
 
 angular.module('app',[])
     .run(function () {
-        console.log('App started ...')
+        console.log('App running ...')
     });
--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,102 @@ var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 // NPM dependencies
-var browserSync = require('browser-sync').create();;
+var argv        = require('yargs').argv;
+var del         = require('del');
+var browserify  = require('browserify');
+var browserSync = require('browser-sync').create();
+var source      = require('vinyl-source-stream');
+var watchify    = require('watchify');
 var wiredep     = require('wiredep').stream;
+
+// Determine run configuration
+// gulp [watch]
+// gulp build
+// gulp build --production
+var build = argv._.length ? argv._[0] === 'build' : false;
+var watch = argv._.length ? argv._[0] === 'watch' : true;
+var production = !!argv['production'];
 
 //
 // Project paths
 var paths = {
-    webRoot: './web/',
-    appStyles: './app/Resources/styles/app.scss'
+    output:     './web/assets/build',
+    styles:     './app/Resources/styles/**/*.scss',
+    appStyles:  './app/Resources/styles/app.scss',
+    scripts:    './app/Resources/scripts/**/*.js',
+    appScripts: './app/Resources/scripts/app.js'
+};
+
+// Task methods
+var tasks = {
+    // Delete output folder
+    clean: function(cb) {
+        del([paths.output + '/']).then(function () {
+            cb();
+        });
+    },
+    // Compile sass sources
+    styles: function () {
+        return gulp.src(paths.appStyles)
+            .pipe(plugins.sass())
+            .pipe(gulp.dest(paths.output + '/css'))
+            .pipe(browserSync.stream());
+    },
+    // Browserif app scripts
+    scripts: function () {
+        var bundler = browserify(paths.appScripts, {
+            debug: !production,
+            cache: {}
+        });
+        if( watch ) {
+            bundler = watchify(bundler);
+        }
+        var rebundle = function() {
+            var pipe =  bundler.bundle()
+                .pipe(source('app.bundle.js'))
+                .pipe(gulp.dest(paths.output + '/js'));
+
+            if( watch ) {
+                pipe.pipe(browserSync.stream({once: true}));
+            }
+            return pipe;
+        };
+        bundler.on('update', rebundle);
+        return rebundle();
+    },
+    // Initialize browser-sync
+    serve: function () {
+        browserSync.init({
+            proxy: 'http://localhost:8000'
+        });
+    },
+    // Watch dependencies
+    watchDeps: function () {
+        // styles
+        gulp.watch(paths.styles, ['styles']);
+        // scripts
+        //gulp.watch(paths.scripts, ['lint'])
+    }
 };
 
 //
-// Main Tasks
+// Main Tasks: build and watch (default)
 //
-
-// Default - npm start
-//   starts the development
-gulp.task('default', ['serve'] );
-
-// Build - npm run build
-//   nuilds the production version
-gulp.task('build');
-
-// The assets pipeline
-var pipes = {};
-
-pipes.stylesDev = function () {
-    return gulp.src(paths.appStyles)
-        .pipe(plugins.sass())
-        .pipe(gulp.dest(paths.webRoot + 'css'))
-        .pipe(browserSync.stream());
-};
+gulp.task('build', ['clean','styles','scripts']);
+gulp.task('watch', ['styles','scripts','serve'], tasks.watchDeps );
+gulp.task('default', ['watch']);
 
 //
-// Helper Tasks
+// Sub Tasks (dependency on clean forced on build tasks)
 //
+var deps = build ? ['clean'] : [];
 
-// Dev Server via browsersync
-gulp.task('serve', function() {
-    browserSync.init({
-        proxy: 'http://localhost:8000'
-    });
-    gulp.watch(paths.appStyles, ['styles:dev']);
-});
+gulp.task('clean', tasks.clean);
+gulp.task('styles', deps, tasks.styles);
+gulp.task('scripts', deps, tasks.scripts);
+gulp.task('serve', tasks.serve);
 
-gulp.task('styles:dev', pipes.stylesDev);
-
-
+// TODO: integrate bootstrap in the toolchain above
 gulp.task('bower', function () {
     gulp.src('./app/Resources/views/base.html.twig')
         .pipe(wiredep({
