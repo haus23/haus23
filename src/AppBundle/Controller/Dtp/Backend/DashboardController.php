27d453888d06bd6ab0ba6f466ca9e71e2d2ea5dd
Micha Alt <micha@haus23.net>
2016-11-03 21:57:46 +0100

    Backend started. Resolves #38

---
+++ src/AppBundle/Controller/Dtp/Backend/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller\Dtp\Backend;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/", name="dtp.dashboard")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('dtp/backend/dashboard.html.twig');
+    }
+}
