035f8bfb112c56c3416a7e56869057c4e0bd3d18
Micha Alt <micha@haus23.net>
2016-11-06 19:19:42 +0100

    Config and code changes to user the new classes.

--- src/AppBundle/Command/CreateUserCommand.php
+++ src/AppBundle/Command/CreateUserCommand.php
@@ -2,7 +2,8 @@
 
 namespace AppBundle\Command;
 
-use AppBundle\CommandBus\FOH\RegisterUser\RegisterUserCommand;
+use Haus23\FOH\Command\RegisterUser;
+use Haus23\FOH\Entity\User;
 use League\Tactician\CommandBus;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\QuestionHelper;
@@ -11,6 +12,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class CreateUserCommand extends ContainerAwareCommand
 {
@@ -23,6 +25,7 @@ class CreateUserCommand extends ContainerAwareCommand
             ->addArgument('username', InputArgument::REQUIRED, 'The username of the user.')
             ->addArgument('email', InputArgument::REQUIRED, 'The email address of the user.')
             ->addOption('admin',null,InputOption::VALUE_NONE,"The user is an admin.",null)
+            ->addOption('nickname',null,InputOption::VALUE_REQUIRED,"User's nickname")
         ;
     }
 
@@ -35,18 +38,27 @@ class CreateUserCommand extends ContainerAwareCommand
         $email = $input->getArgument('email');
 
         $isAdmin = $input->getOption('admin');
-        $groups = ['ROLE_USER'];
+        $roles = ['ROLE_USER'];
         if( $isAdmin ) {
-            array_push($groups, 'ROLE_ADMIN');
+            array_push($roles, 'ROLE_ADMIN');
+        }
+
+        $nickname = null;
+        if( $input->hasOption('nickname') ) {
+            $nickname = $input->getOption('nickname');
         }
 
         $question = new Question('Password? ');
         $question->setHidden(true);
         $password = $helper->ask($input, $output, $question);
 
+        /** @var UserPasswordEncoder $encoder */
+        $encoder = $this->getContainer()->get('security.password_encoder');
+        $passwordHash = $encoder->encodePassword(new \AppBundle\Security\User($email, $username, $password, $roles),$password);
+
         /** @var CommandBus $commandBus */
         $commandBus = $this->getContainer()->get('tactician.commandbus');
-        $cmd = new RegisterUserCommand($username, $email, $password, $groups);
+        $cmd = new RegisterUser($username, $passwordHash, $email, $nickname, $roles);
         $commandBus->handle($cmd);
     }
 }
