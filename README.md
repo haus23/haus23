6943147ad8c7df60efb12666cc376fd22bce6440
Micha Alt <micha@haus23.net>
2016-12-08 18:10:55 +0100

    DTP db configured and first root entity created: Team

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Website https://haus23.net
+
+## Databases
+
+### DTP
+
+    ./bin/console doctrine:database:create --connection=dtp
+    ./bin/console doctrine:generate:entities --path src AppBundle/Entity/DTP
+    ./bin/console doctrine:schema:update --dump-sql --em dtp --force
+
---
+++ src/AppBundle/Entity/DTP/Team.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Entity\DTP;
+
+/**
+ * Team
+ */
+class Team
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $shortname;
+
+    /**
+     * @var string
+     */
+    private $slug;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Team
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set shortname
+     *
+     * @param string $shortname
+     *
+     * @return Team
+     */
+    public function setShortname($shortname)
+    {
+        $this->shortname = $shortname;
+
+        return $this;
+    }
+
+    /**
+     * Get shortname
+     *
+     * @return string
+     */
+    public function getShortname()
+    {
+        return $this->shortname;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Team
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
